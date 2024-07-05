fa890f1868311f5ac8defcc3c604a0f9c750e694
kehinde-omotoso-78financials <110571589+kehinde-omotoso-78financials@users.noreply.github.com>
2024-07-06 00:30:14 +0100

    Sync from Kehinde

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,7 @@ const today = new Date();
 
 document.getElementById("currentyear").textContent = today.getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
+
+const setTextNode = document.getElementById("lastModified");
+setTextNode.textContent = document.lastModified;
+setTextNode.inn
